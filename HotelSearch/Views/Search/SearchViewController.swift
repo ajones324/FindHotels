4e11a975b12763bbd77ed104a6164968e7d9daa2
Andrew Jones <ajones20324@gmail.com>
2022-11-29 03:45:36 -0600

    Find hotels with REST API, URLSession and Combine
    Find hotels with REST API, URLSession, building UI layouts programmatically with Auto-layout and Combine.

---
+++ HotelSearch/Views/Search/SearchViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SearchViewController.swift
+//  HotelSearch
+//
+//  Created by Andrew Ikenna Jones on 10/31/22.
+//
+
+import UIKit
+import Combine
+
+class SearchViewController: UIViewController {
+
+    private var viewModel: SearchViewModel = SearchViewModel(title: "Hotels")
+    private var cancellables: Set<AnyCancellable> = []
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        bindViewModel()
+        prepareView()
+    }
+    
+    private func prepareView() {
+        self.title = viewModel.title
+        self.searchBar.searchTextField.autocapitalizationType = .words
+        self.searchBar.returnKeyType = .search
+    }
+    
+    private func bindViewModel() {
+        viewModel.$hotels
+            .sink { _ in
+                DispatchQueue.main.async {
+                    self.updateHotels()
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func updateHotels() {
+        self.tableView.reloadData()
+    }
+}
+
+// MARK: UISearchBarDelegate Methods
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if let text = searchBar.text, text.isEmpty {
+            viewModel.clearHotels()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let query = searchBar.text,
+           !query.isEmpty {
+            viewModel.fetchHotels(query: query)
+            searchBar.resignFirstResponder()
+        }
+    }
+}
+
+// MARK: TableViewDataSource, TableViewDelegate Methods
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.hotels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let hotelCell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+        hotelCell.accessoryType = .detailDisclosureButton
+        let hotel = viewModel.hotels[indexPath.row]
+        hotelCell.textLabel?.text = hotel.displayName()
+        return hotelCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.reloadRows(at: [indexPath], with: .fade)
+        let selected = viewModel.hotels[indexPath.row]
+        let detailVC = HotelDetailViewController(viewModel: HotelDetailViewModel(model: selected, title: ""))
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
---
+++ HotelSearch/Views/Search/SearchViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  SearchViewModel.swift
+//  HotelSearch
+//
+//  Created by Andrew Ikenna Jones on 10/31/22.
+//
+
+import Foundation
+import Combine
+
+
+final class SearchViewModel: ObservableObject {
+    let apiService: LocationService
+    @Published private(set) var hotels: [LocationItem] = []
+    var title: String
+
+    init(model:[LocationItem]? = nil, title: String) {
+        if let model = model {
+            self.hotels = model
+        }
+        self.apiService = LocationService()
+        self.title = title
+    }
+}
+
+extension SearchViewModel {
+    func fetchHotels(query: String) {
+        self.apiService.searchHotels(query: query) { result in
+            switch result {
+            case .success(let hotels):
+                self.hotels = hotels
+                return
+            case .failure(_):
+                return
+            }
+        }
+    }
+    
+    func clearHotels() {
+        hotels.removeAll()
+    }
+}
